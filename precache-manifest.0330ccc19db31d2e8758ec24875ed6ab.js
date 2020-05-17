c7915e945f88db9cbd0e5fe9325c3b4768c819ef
0yah <42600121+0yah@users.noreply.github.com>
2020-05-17 08:21:22 +0300

    Updates

---
+++ precache-manifest.0330ccc19db31d2e8758ec24875ed6ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13ce88eb0fe42045434c341b741bc86",
+    "url": "/animals-kenya/index.html"
+  },
+  {
+    "revision": "f3f7a4094a8896b293f2",
+    "url": "/animals-kenya/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a51d86cac61af8aa2a8",
+    "url": "/animals-kenya/static/js/2.dd17b46f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animals-kenya/static/js/2.dd17b46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f7a4094a8896b293f2",
+    "url": "/animals-kenya/static/js/main.5ec5cd7f.chunk.js"
+  },
+  {
+    "revision": "88f3c8656b11da9d7b45",
+    "url": "/animals-kenya/static/js/runtime-main.23604901.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animals-kenya/static/media/logo.5d5d9eef.svg"
+  }
+]);
